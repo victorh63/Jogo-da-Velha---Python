90cd40dd4c575b459426542ef99f47db01014d53
Victor <vichugo251106@hotmail.com>
2025-07-22 20:45:38 -0300

    Commit 1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 """
 Jogo da Velha em Python
 """
+
+def mostrar_tabuleiro(tab):
+    for linha in tab:
+        print(" | ".join(linha))
+
+tabuleiro = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+
+mostrar_tabuleiro(tabuleiro)
