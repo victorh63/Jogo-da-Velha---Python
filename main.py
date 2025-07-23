cabc44740225e5d524534ebd29075314ba409f80
Victor <vichugo251106@hotmail.com>
2025-07-22 21:24:08 -0300

    Commit 2

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ def mostrar_tabuleiro(tab):
     for linha in tab:
         print(" | ".join(linha))
 
-tabuleiro = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+tabuleiro = [["1", "2", "3"], 
+             ["4", "5", "6"], 
+             ["7", "8", "9"]]
+jogador_atual = "X"
 
-mostrar_tabuleiro(tabuleiro)
+for rodada in range(9):
+    mostrar_tabuleiro(tabuleiro)
+    escolha = input(f"Jogador {jogador_atual}, escolha uma posição de (1-9): ")
+    pos = int(escolha) - 1
+    linha, coluna = pos // 3, pos % 3
+
+    if tabuleiro[linha][coluna] in ["X", "O"]:
+        print("Posição já ocupada. Tente outra.")
+        continue
+
+    tabuleiro[linha][coluna] = jogador_atual
+
+    if jogador_atual == "O":
+        jogador_atual = "X"
+    else:
+        jogador_atual = "O"
