f8ab8f3584b3f345ea740179d7a6095b6a57f56f
Victor <vichugo251106@hotmail.com>
2025-07-22 21:40:10 -0300

    Commit 3

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ def mostrar_tabuleiro(tab):
     for linha in tab:
         print(" | ".join(linha))
 
+def verificar_vitoria(tab, jogador):
+    #Verifica as linhas
+    for i in range(3):
+        if tab[i][0] == jogador and tab[i][1] == jogador and tab[i][2] == jogador:
+            return True
+        
+    #Verifica as colunas
+    for i in range(3):
+        if tab[0][i] == jogador and tab[1][i] == jogador and tab[2][i] == jogador:
+            return True
+        
+    if tab[0][0] == jogador and tab[1][1] == jogador and tab[2][2] == jogador:
+        return True
+
+    if tab[0][2] == jogador and tab[1][1] == jogador and tab[2][0] == jogador:
+        return True
+
+    return False
+
 tabuleiro = [["1", "2", "3"], 
              ["4", "5", "6"], 
              ["7", "8", "9"]]
@@ -22,6 +41,11 @@ for rodada in range(9):
         continue
 
     tabuleiro[linha][coluna] = jogador_atual
+
+    if verificar_vitoria(tabuleiro, jogador_atual):
+        mostrar_tabuleiro(tabuleiro)
+        print(f"Jogador {jogador_atual} venceu!")
+        break
 
     if jogador_atual == "O":
         jogador_atual = "X"
